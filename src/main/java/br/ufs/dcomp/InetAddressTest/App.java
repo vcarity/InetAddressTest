abc92cb0ee82eed8bd82d548986c4733f2049c2d
Victor Carity <victorcfsc@dcomp.ufs.br>
2017-11-28 19:15:37 +0000

    Inet getAllByName

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -2,20 +2,21 @@ package br.ufs.dcomp.InetAddressTest;
 
 import java.net.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         try{
             
-            String name = "www.ufs.br";
-            InetAddress address = InetAddress.getByName(name); 
+            String name = "www.google.com";
+            
+            InetAddress[] address_list = InetAddress.getAllByName(name);
+    
             System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address);
+            for (int i = 0; i < address_list.length; i++) {
+                InetAddress address = address_list[i];
+                System.out.println( "Address:   "+ address.getHostAddress());
+            }
 
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
